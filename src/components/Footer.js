03fdea34dd92f2822c95e5b95f0e4eebabd7444a
JessicaLDaley <jessicadaleypaints@yahoo.com>
2022-01-17 17:22:20 -0500

    portfolio cards responsive but margin still needs work

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         <p>&nbsp;</p>
         <p>&nbsp;</p>
     <div className="bg-dark text-white fixed-bottom footer mt-auto py-3">
-    <span className="text-muted"> Jessica Daley &bull; 2021 &bull; MIT license</span>
+    <span className="text-muted"> Jessica Daley &bull; 2022 &bull; MIT license</span>
     </div>
     </div>
     );
